2c65cfcd9efd7f3d165d65da0945e881e47efb70
OtBot <info@o-blog.info>
2024-06-20 17:23:41 +0200

    ui

--- frontend/src/components/ui/BackupButton.jsx
+++ frontend/src/components/ui/BackupButton.jsx
@@ -2,15 +2,16 @@ import { Button, Col, Row } from "react-bootstrap";
 import React from "react";
 import "../ui/css/BackupButton.css";
 
-const BackupButton = ({ id, name, callbackRestore, callbackDelete }) => {
+const BackupButton = ({ id, name, date, callbackRestore, callbackDelete }) => {
 
   return (
     <Row className={ "p-3 mx-1 my-2 bg-primary backupbutton-row" }>
-        <Col md={ 3 } >
+        <Col md={ 2 } >
           <Button variant={ "outline-success" } className={ " me-2" }
                   onClick={ () => callbackRestore(id, name) }>Restore </Button>
           <Button variant={ "outline-danger" } onClick={ () => callbackDelete(id) }>Delete</Button>
         </Col>
+        <Col md={2}>{date}</Col>
         <Col>
           <a className={ "backupbutton-text" } href={`${process.env.REACT_APP_BACKEND_URL}/media/backup/${name}`}>{ name }</a>
         </Col>
